732f0fd8c9c2f01bd0a3480e5258aa08b1350e57
Korgaonkar <siddharth.korgaonkar@sogeti.com>
2020-06-26 11:36:42 +0200

    tooltip

--- src/components/Bouquet/FlowerControls/FlowerControls.js
+++ src/components/Bouquet/FlowerControls/FlowerControls.js
@@ -7,18 +7,22 @@ import classes from './FlowerControls.css';
 const flowerControls = (props) => (
     <div className={classes.FlowerControls}>
         <div className={classes.Orderdata}>
-            <h1 className={classes.Heading}> Smart Basket </h1>
+            <h1 className={classes.Heading}> Smart Bouquet </h1>
             <p className={classes.Ordertext}>
                 Make your Flowers shopping easy, breezy &amp; smart! See all the items you have bought or liked in one place.
         </p>
         </div>
         <div className={classes.Buy}>
-        <p className={classes.Total}>Total Price : $ <strong>{props.price.toFixed(2)}</strong></p>
+            <p className={classes.Total}>Total Price : $ <strong>{props.price.toFixed(2)}</strong></p>
 
-        <button className={classes.OrderButton}
-            disabled={!props.purchasable}
+            <button className={classes.OrderButton}
+                disabled={!props.purchasable}
 
-            onClick={props.ordered} >{ props.isAuth ? 'ORDER NOW' : 'SIGNUP For Order'}</button>
+                onClick={props.ordered}  >
+                {props.isAuth ? 'ORDER NOW' : 'SIGNUP For Order'}
+                {/* <span className={classes.Tooltip}>{!props.isAuth ? 'Add Flowers in Bouquet' : ''}</span> */}
+                <span className={classes.Tooltip}>Add Some Flowers from ABOVE</span>
+            </button>
 
         </div>
     </div>
